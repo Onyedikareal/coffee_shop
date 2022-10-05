cfc40e850984f19f18f809b15b17fa0bcb0a74a4
Onyedikareal <frankonyeka25@gmail.com>
2022-10-05 02:01:51 +0100

    coffee_shop

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,15 @@
+/* @TODO replace with your variables
+ * ensure all variables on this page match your project
+ */
+
+
+export const environment = {
+  production: false,
+  apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
+  auth0: {
+    url: 'dev-6l-5njji.us', // the auth0 domain prefix
+    audience: 'coffeeshop', // the audience set for the auth0 app
+    clientId: 'zKIRzC0bhpJGwNv6tmlnmwX14YAU0J4o', // the client id generated for the auth0 app
+    callbackURL: 'https://127.0.0.1:8100', // the base url of the running ionic application. 
+  }
+};
